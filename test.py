0838d9f18df00641030d549803d706d826aa1a68
juanbaezl <60081580+juanbaezl@users.noreply.github.com>
2020-01-30 15:05:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,37 +8,37 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual (Suma (v,d) , (7,6))
 
 
-def test_resta(self):
-          v = (3,1)
-          d = (4,5)
-          self.assertEqual (resta (v,d) , (-1,-4))
-        
-def test_multiplicación(self):
-        v = (3,1)
-        d = (4,5)
-        self.assertEqual (mult (v,d) , (7,19))
+    def test_resta(self):
+              v = (3,1)
+              d = (4,5)
+              self.assertEqual (resta (v,d) , (-1,-4))
 
-def test_división(self):
-        v = (1,3)
-        d = (1,1)
-        self.assertEqual (div (v,d) , (2,1))
+    def test_multiplicación(self):
+            v = (3,1)
+            d = (4,5)
+            self.assertEqual (mult (v,d) , (7,19))
 
-def test_conjugada(self):
-        v = (3,1)
-        self.assertEqual (conjug (v) , (3,-1))
+    def test_división(self):
+            v = (1,3)
+            d = (1,1)
+            self.assertEqual (div (v,d) , (2,1))
 
-def test_modulo(self):
-        v = (3,1)
-        self.assertEqual (mod (v) , (10**(1/2)))
+    def test_conjugada(self):
+            v = (3,1)
+            self.assertEqual (conjug (v) , (3,-1))
 
-def test_fase (self) :
-    v = (1,1)
-    self.assertEqual (retorno (v) , (45))
+    def test_modulo(self):
+            v = (3,1)
+            self.assertEqual (mod (v) , (10**(1/2)))
 
-def test_polar (self):
-    v = (1,1)
-    self.assertEqual (pol (v) , ((2**(1/2)) , 45))
+    def test_fase (self) :
+        v = (1,1)
+        self.assertEqual (fase (v) , (45))
+
+    def test_polar (self):
+        v = (1,1)
+        self.assertEqual (pol (v) , ((2**(1/2)) , 45))
 
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
